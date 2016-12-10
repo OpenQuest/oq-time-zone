74df363a0659ef5bcc5a5e2cceadcb100321d408
lwj <1198275198@qq.com>
2016-12-10 23:19:50 +0800

    test

--- test.js
+++ test.js
@@ -2,13 +2,37 @@
 
 var mocha = require('mocha');
 var chai = require('chai');
-var [func] = require('./index.js');
+var timeZone = require('./index.js');
 
 var expect = chai.expect;
 
-describe('', function() {
+describe('Time Zone', function() {
 
     it('should export a function', function() {
-        expect([func]).to.be.a('function');
+        expect(timeZone).to.be.a('function');
+    });
+
+    it('should get +8 when localtime is in BeiJing', function() {
+        expect(timeZone()).to.equal('+8');
+    });
+
+    it('should get +6:30 when localtime is in Burma', function() {
+        expect(timeZone()).to.equal('+6:30');
+    });
+
+    it('should get +1 when localtime is in Norway but not daylight-saving time', function() {
+        expect(timeZone()).to.equal('+1');
+    });
+
+    it('should get +2 when localtime is in Norway and in daylight-saving time', function() {
+        expect(timeZone(new Date(2016, 5, 1))).to.equal('+2');
+    });
+
+    it('should get 0 when localtime is in England', function() {
+        expect(timeZone()).to.equal('0');
+    });
+
+    it('should get right formatted string', function() {
+        expect(timeZone()).to.match(/^[+-]?[\d:]+$/);
     });
 });
